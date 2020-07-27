53bfc7f9805cb9be27da4428fe60cb8827c88327
chuguanwu666 <zxc8332815>
2020-07-27 10:48:29 +0800

    第二次提交

---
+++ src/domain/Store.java
@@ -0,0 +1,42 @@
+package domain;
+
+public class Store {
+    private String name;
+    private String address;
+    private int tel;
+    private String dis;
+    private int i;
+
+    public String getName() {
+
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getTel() {
+        return tel;
+    }
+
+    public void setTel(int tel) {
+        this.tel = tel;
+    }
+
+    public String getDis() {
+        return dis;
+    }
+
+    public void setDis(String dis) {
+        this.dis = dis;
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,13 @@
+
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  <hr>
+  <form action="${pageContext.request.contextPath}/serachService">
+  <input type="text">  <button type="submit" >搜索</button>
+  </form>
+  </body>
+</html>
